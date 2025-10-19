3d45e89c638abeda6145b8c39970e49070877cc9
Daniel Moncada <162639596+danielmoncada7@users.noreply.github.com>
2025-10-19 18:53:21 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+# Root endpoint
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# Get endpoint
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+# Put endpoint for updating an item
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
